d4c3525ca1ae30e71181e53399c008b60e277b99
laynath <laynath2018@gmail.com>
2020-11-08 14:45:07 +0700

    add load contract

--- indracore_api/src/contract/deploy.rs
+++ indracore_api/src/contract/deploy.rs
@@ -1,9 +1,25 @@
 use crate::primitives;
-use std::path::PathBuf;
+use std::{fs, io::Read, path::PathBuf};
 
 pub struct ContractDeploy {
     pub wasm_path: PathBuf,
     pub signer: primitives::Signer,
 }
+use substrate_subxt::Error;
 
-impl ConractDeploy {}
+impl ContractDeploy {
+    pub fn load_contract(&self) -> Result<Vec<u8>, Error> {
+        let contract_wasm_path = self.wasm_path.clone();
+        let mut data: Vec<u8> = Vec::new();
+
+        let file = fs::File::open(&contract_wasm_path);
+        let mut file = match file {
+            Ok(f) => f,
+            Err(_) => return Err(Error::Other("File not exit".to_string())),
+        };
+        match file.read_to_end(&mut data) {
+            Ok(_) => Ok(data),
+            Err(_) => Err(Error::Other("File Cannot be read".to_string())),
+        }
+    }
+}

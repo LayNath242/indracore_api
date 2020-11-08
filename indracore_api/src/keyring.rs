6ed3cabdc1b94ff5bf59c9c78cc9e9e9c4036cd0
laynath <laynath2018@gmail.com>
2020-11-08 16:16:28 +0700

    add code hash from string

--- indracore_api/src/keyring.rs
+++ indracore_api/src/keyring.rs
@@ -1,6 +1,7 @@
 use crate::primitives;
 use substrate_subxt::{
     sp_core::{sr25519::Pair, Pair as TraitPair},
+    system::System,
     Error, IndracoreNodeRuntime, PairSigner,
 };
 
@@ -17,4 +18,33 @@ impl Signer {
             Err(_) => Err(Error::Other("Invalid account".into())),
         }
     }
+}
+
+pub fn parse_code_hash(
+    input: &str,
+) -> Result<<IndracoreNodeRuntime as System>::Hash, hex::FromHexError> {
+    let bytes = if input.starts_with("0x") {
+        hex::decode(input.trim_start_matches("0x"))?
+    } else {
+        hex::decode(input)?
+    };
+    if bytes.len() != 32 {
+        return Err(hex::FromHexError::InvalidStringLength);
+    }
+    let mut arr = [0u8; 32];
+    arr.copy_from_slice(&bytes);
+    Ok(arr.into())
+}
+
+#[test]
+fn parse_code_hash_works() {
+    // with 0x prefix
+    assert!(
+        parse_code_hash("0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d")
+            .is_ok()
+    );
+    // without 0x prefix
+    assert!(
+        parse_code_hash("d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d").is_ok()
+    )
 }

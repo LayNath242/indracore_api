73a3f37c840d340f7f5ae27f0dd62025ad2a097b
laynath <laynath2018@gmail.com>
2020-11-08 13:45:35 +0700

    add load metadata

--- indracore_api/src/contract/mod.rs
+++ indracore_api/src/contract/mod.rs
@@ -1,1 +1,11 @@
-pub mod transcode;
+mod transcode;
+
+use anyhow::Result;
+use std::{fs::File, path::PathBuf};
+
+pub fn load_metadata() -> Result<ink_metadata::InkProject> {
+    let mut path: PathBuf = PathBuf::new();
+    path.push("/data/project/cr/template/erc20/target/metadata.json");
+    let metadata = serde_json::from_reader(File::open(path)?)?;
+    Ok(metadata)
+}

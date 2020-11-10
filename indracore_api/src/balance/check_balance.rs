b2bdb8c8487458e9ff9df1da4e62096688a4f399
laynath <laynath2018@gmail.com>
2020-11-10 10:09:43 +0700

    check balance

--- indracore_api/src/balance/check_balance.rs
+++ indracore_api/src/balance/check_balance.rs
@@ -1,5 +1,15 @@
 use crate::primitives;
-use substrate_subxt::{balances::*, ClientBuilder, Error, IndracoreNodeRuntime};
+use substrate_subxt::{
+    balances::*, sp_core::crypto::AccountId32, system::*, ClientBuilder, Error,
+    IndracoreNodeRuntime,
+};
+
+pub struct BalanceInfo {
+    pub free: u128,
+    pub reserved: u128,
+    pub misc_frozen: u128,
+    pub fee_frozen: u128,
+}
 
 pub async fn total_issuance() -> Result<u128, Error> {
     let client = match ClientBuilder::<IndracoreNodeRuntime>::new()
@@ -15,4 +25,45 @@ pub async fn total_issuance() -> Result<u128, Error> {
         Err(e) => return Err(e),
     };
     Ok(total)
+}
+
+pub async fn free_balance(id: AccountId32) -> Result<u128, Error> {
+    let client = match ClientBuilder::<IndracoreNodeRuntime>::new()
+        .set_url(primitives::url())
+        .build()
+        .await
+    {
+        Ok(cli) => cli,
+        Err(e) => return Err(e),
+    };
+
+    let info = match client.account(&id, None).await {
+        Ok(info) => info,
+        Err(e) => return Err(e),
+    };
+    println!("{:?}", info);
+    Ok(info.data.free)
+}
+
+pub async fn balance_info(id: AccountId32) -> Result<BalanceInfo, Error> {
+    let client = match ClientBuilder::<IndracoreNodeRuntime>::new()
+        .set_url(primitives::url())
+        .build()
+        .await
+    {
+        Ok(cli) => cli,
+        Err(e) => return Err(e),
+    };
+
+    let info = match client.account(&id, None).await {
+        Ok(info) => info,
+        Err(e) => return Err(e),
+    };
+
+    Ok(BalanceInfo {
+        free: info.data.free,
+        misc_frozen: info.data.misc_frozen,
+        reserved: info.data.reserved,
+        fee_frozen: info.data.fee_frozen,
+    })
 }
